ab07a0c4317f9adb2db14b9ba31cfa67b12ac498
dennisappiahALX <appiahdennis1998@gmail.com>
2024-01-23 17:40:05 +0000

    db

---
+++ HotelAppDB/dbo/Tables/RoomTypes.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[RoomTypes]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Title] NCHAR(50) NOT NULL, 
+    [Description] NCHAR(200) NOT NULL, 
+    [Price] MONEY NOT NULL
+)
